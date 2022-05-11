40b14728ee05d9d4a09926fbef84c196def7c492
vitor vigatti <105285124+vitorvigatti@users.noreply.github.com>
2022-05-11 09:37:22 -0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,5 @@
+function mudartexto(){
+     //modificando o botao vila id//
+    document.getElementById("botao").innerHTML = "botao pra se tornar um otaku falido"
+    alert("toma cuidado pra nao levar um hakai satsu no meio da cara")
+}
