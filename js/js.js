da229810153adf92ee1ce7389248112d070a0cf3
vitor vigatti <105285124+vitorvigatti@users.noreply.github.com>
2022-05-25 07:33:09 -0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -3,3 +3,19 @@ function mudartexto(){
     document.getElementById("botao").innerHTML = "botao pra se tornar um otaku falido"
     alert("toma cuidado pra nao levar um hakai satsu no meio da cara")
 }
+
+if (equacao) {
+    try{
+        Input.innerHTML = eval (equacao)
+    } catch (e) {
+        alert ('erro na expressao');
+    }
+}
+break;
+case 67;
+case 99;
+      Input.innerHTML = "";
+      break;
+      default;
+      break;
+      
